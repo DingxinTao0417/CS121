a21eaf443db784fb9da2ae1a5a3cc25e3b947409
Dingxin Tao <taodingxin0417@gmail.com>
2025-04-14 23:20:09 -0700

    add comments

--- Assignment1/PartA.py
+++ Assignment1/PartA.py
@@ -1,10 +1,14 @@
 import sys
 
-def tokenize(Path) -> list:
+# tokenize(), param: Path - string, return: tokens - list
+# Complexity: O(n), n here is the number of characters in the input file.
+# tokenize function reads the input file line by line and processes every character
+def tokenize(Path: str) -> list:
     tokens = []
 
-    # is_valid function accept a character to determine if
-    # it's a valid for english or num char
+    # is_valid(), param: c - string, return: boolean
+    # Complexity: O(1)
+    # is_valid function accept a character to determine if it's a valid for english or num char
     def is_valid(c: str) -> bool:
         return ('a' <= c <= 'z') or ('A' <= c <= 'Z') or ('0' <= c <= '9') or c == '_'
 
@@ -23,20 +27,37 @@ def tokenize(Path) -> list:
 
     return tokens
 
+# computeWordFrequencies(), param: tokens - list, return: freq - dict
+# Complexity: O(n), n here is the number of elements in tokens list.
+# computeWordFrequencies function processes each element in tokens, put them into frequency dict.
+# key is the word in tokens, value is the frequency of the word appeared in tokens list.
 def computeWordFrequencies(tokens: list) -> dict:
     freq = {}
     for token in tokens:
-        if token not in freq:
-            freq[token] = 1
-        else:
+        if token in freq:
             freq[token] += 1
+        else:
+            freq[token] = 1
     return freq
 
+# print_frequencies(), param: freq_map - dict, return: None
+# Complexity: O(nlogn), n here is the number of keys in freq_map dictionary
+# O(nlogn) is for python sorted function. Python sorted is Tim-Sort.
+# Tim-Sort: Tim-Sort will split the array into sub arrays(runs) with a certain size
+# For those runs, timsort uses insertion sort to sort the sub array，
+# insertion sort in timsort time complexity is O(n)
+# and then uses merge sort to combine those runs into a whole sorted array
+# merge sort in timsort time complexity is O(nlogn), in total the timsort time complexity will be O(nlogn)
+# It runs O(nlogn) for the best case. For worst case, it will be O(n).
+# O(n) is for getting each key and value pair in sorted_freq map. n is the total number of keys in the map.
+# Therefore, the overall time complexity will be O(nlogn) by Timsort.
+# print_frequencies function prints every pair in freq_map to command line.
 def print_frequencies(freq_map: dict) -> None:
     sorted_freq = sorted(freq_map.items(), key=lambda x: x[1], reverse=True)
-    for key,value in sorted_freq:
+    for key, value in sorted_freq:
         print(key+' -> '+str(value))
 
+# main function, checks the number of arguments from command line, raises error if there's bad argument
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Has to follow the format: python PartA.py <filepath>")
--- Assignment1/PartB.py
+++ Assignment1/PartB.py
@@ -1,7 +1,10 @@
 import sys
 import PartA as PA
 
-def fine_common_tokens(file1, file2):
+# find_common_tokens(), param: file1, file2(path of these two files) - string, return: length of common tokens integer
+# Complexity: O(n+m), n and m are the number of characters in each file, tokenize function reads and tokenizes file linearly
+# find_common_tokens function uses set intersection of two token lists, and count the number of common token in two lists.
+def find_common_tokens(file1, file2):
     tokens1 = set(PA.tokenize(file1))
     tokens2 = set(PA.tokenize(file2))
     common_tokens = tokens1 & tokens2
@@ -13,5 +16,5 @@ if __name__ == '__main__':
 
     file1 = sys.argv[1]
     file2 = sys.argv[2]
-    common_tokens = fine_common_tokens(file1, file2)
+    common_tokens = find_common_tokens(file1, file2)
     print(common_tokens)
